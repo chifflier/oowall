8d19c2ce013c89b4797b330d67b8e39ffb84d038
Eric Leblond <eleblond@inl.fr>
2010-07-10 12:54:22 +0200

    On the fly packet modification
    This patch modifies oowall to make use of the second tab in the
    spreadsheet to do on the fly substitution of words.

--- oowall.py
+++ oowall.py
@@ -7,7 +7,7 @@
 from xmlrpclib import ServerProxy
 from random import randint
 import nfqueue
-from dpkt import ip
+from dpkt import ip, tcp
 from socket import AF_INET, AF_INET6, inet_ntoa
 import sys
 
@@ -47,16 +47,46 @@ def update_stats_for_port(dport, pkt, is_allowed):
     cell_value += 1
     oo_instance.setCell(session, book, sheet, oo_index, idx, cell_value)
 
+def get_list_of_words():
+    ret = dict()
+    y = 1
+    while True:
+        cell_word = oo_instance.getCell(session, book, sheet + 1, 0, y)
+        if not cell_word:
+            break
+        cell_translation = oo_instance.getCell(session, book, sheet + 1, 1, y)
+        cell_translated = oo_instance.getCell(session, book, sheet + 1, 2, y)
+        ret[cell_word] = (y, cell_translation, cell_translated)
+        y += 1
+    return ret
+
+def update_stats_for_word(word):
+    # get index
+    val = oo_words[word]
+    idx = val[IDX_IDX]
+    oo_index = 2
+    # get cell value
+    cell_value = oo_instance.getCell(session, book, sheet + 1, oo_index, idx)
+    if not cell_value:
+        cell_value = 0
+    print "previous cell value: ", cell_value
+    # increment, then update
+    cell_value += 1
+    oo_instance.setCell(session, book, sheet + 1, oo_index, idx, cell_value)
+
 def cb(i,payload):
-        global pkt_counter, oo_tcp_ports
+        global pkt_counter, oo_tcp_ports, oo_words
         # every n packets, re-read list of ports
         if pkt_counter % 50 == 0:
             print "re-reading list of TCP ports"
             oo_tcp_ports = get_list_of_tcp_ports()
+            print "re-reading list of words"
+            oo_words = get_list_of_words()
         pkt_counter += 1
         #print "payload len ", payload.get_length()
         data = payload.get_data()
         pkt = ip.IP(data)
+        decision = nfqueue.NF_DROP
         #print "proto:", pkt.p
         #print "source: %s" % inet_ntoa(pkt.src)
         #print "dest: %s" % inet_ntoa(pkt.dst)
@@ -69,13 +99,43 @@ def cb(i,payload):
                     if t[IDX_VERDICT] == 1:
                         print "Port %d allowed" % dport
                         update_stats_for_port(dport, pkt, 1)
-                        payload.set_verdict(nfqueue.NF_ACCEPT)
+                        decision = nfqueue.NF_ACCEPT
+                # don't check for packet return on SYN
+                if pkt.tcp.flags & tcp.TH_SYN and not pkt.tcp.flags & tcp.TH_ACK:
+                    payload.set_verdict(decision)
+                    sys.stdout.flush()
+                    return 1
+                # crappy reverse check
+                sport = pkt.tcp.sport
+                if sport in oo_tcp_ports and dport >= 1024:
+                    t = oo_tcp_ports[sport]
+                    if t[IDX_VERDICT] == 1:
+                        print "S Port %d allowed" % sport
+                        #update_stats_for_port(dport, pkt, 1)
+                        decision = nfqueue.NF_ACCEPT
                     else:
                         print "Port %d dropped" % dport
-                        update_stats_for_port(dport, pkt, 0)
+                        #update_stats_for_port(dport, pkt, 0)
                         payload.set_verdict(nfqueue.NF_DROP)
-                else:
-                    print "Port %d not known, so dropped" % dport
+                        sys.stdout.flush()
+                        return 1
+                # accepted data packet need to be modified
+                if pkt.tcp.flags & tcp.TH_PUSH:
+                    pkt2 = pkt
+                    for word in oo_words.keys():
+                        if str(pkt.tcp.data).find(word) != -1:
+                            # Do substitution
+                            print "Found %s word" % word
+                            print pkt2.tcp.data
+                            old_len = len(pkt2.tcp.data)
+                            pkt2.tcp.data = str(pkt2.tcp.data).replace(word,oo_words[word][1])
+                            pkt2.len = pkt2.len - old_len + len(pkt2.tcp.data)
+                            pkt2.tcp.sum = 0
+                            pkt2.sum = 0
+                            update_stats_for_word(word)
+
+                    payload.set_verdict_modified(nfqueue.NF_ACCEPT,str(pkt2),len(pkt2))
+                elif decision == nfqueue.NF_DROP:
                     payload.set_verdict(nfqueue.NF_DROP)
         payload.set_verdict(nfqueue.NF_ACCEPT)
 
@@ -91,7 +151,7 @@ q.fast_open(0, AF_INET)
 oo_instance = ServerProxy('http://localhost:8000')
 
 session = oo_instance.openSession('string')
-book = oo_instance.openBook(session, '/home/pollux/oowall.ods')
+book = oo_instance.openBook(session, '/home/eric/git/oowall/oowall.ods')
 
 sheet_list = oo_instance.getBookSheets(session, book)
 print sheet_list
@@ -106,6 +166,7 @@ print "sheet ", sheet
 
 pkt_counter = 0
 oo_tcp_ports = get_list_of_tcp_ports()
+oo_words = get_list_of_words()
 
 q.set_queue_maxlen(5000)
 
